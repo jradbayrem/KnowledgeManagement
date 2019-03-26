be7a38c05a16497d074a568c7f0724307cb92e52
Jrad Bayrem <bjrad@adaming.fr>
2019-03-26 11:52:35 +0100

    Create project's structure

---
+++ src/main/java/com/wemanity/KnowledgeManagement/KnowledgeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wemanity.KnowledgeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeManagementApplication.class, args);
+	}
+
+}
