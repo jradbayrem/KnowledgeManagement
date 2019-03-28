db0e4c156218172bb0bda2a47658df12f06bed82
Jrad Bayrem <bjrad@adaming.fr>
2019-03-28 17:30:04 +0100

    Adding findByRelatedProject and findByUserCreator to IKnowledgeRepository

--- src/main/java/com/wemanity/KnowledgeManagement/repositories/IKnowledgeRepository.java
+++ src/main/java/com/wemanity/KnowledgeManagement/repositories/IKnowledgeRepository.java
@@ -1,9 +1,17 @@
 package com.wemanity.KnowledgeManagement.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wemanity.KnowledgeManagement.entities.Knowledge;
+import com.wemanity.KnowledgeManagement.entities.Project;
+import com.wemanity.KnowledgeManagement.entities.User;
 
 public interface IKnowledgeRepository extends JpaRepository<Knowledge, Integer> {
+	
+	public List<Knowledge> findByRelatedProject(Project project);
+	
+	public List<Knowledge> findByUserCreator(User user);
 
 }
