4362461faad698edd80cee5b04fa7751ac2f71ed
Jrad Bayrem <bjrad@adaming.fr>
2019-03-28 15:48:57 +0100

    Addind the findByKnowledge method in the repository

--- src/main/java/com/wemanity/KnowledgeManagement/repositories/ICommentRepository.java
+++ src/main/java/com/wemanity/KnowledgeManagement/repositories/ICommentRepository.java
@@ -1,9 +1,14 @@
 package com.wemanity.KnowledgeManagement.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wemanity.KnowledgeManagement.entities.Comment;
+import com.wemanity.KnowledgeManagement.entities.Knowledge;
 
 public interface ICommentRepository extends JpaRepository<Comment, Integer> {
+	
+	public List<Comment> findByKnowledge(Knowledge knowldge);
 
 }
