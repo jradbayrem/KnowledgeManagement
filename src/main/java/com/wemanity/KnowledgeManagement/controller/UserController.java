130d22b68d93da48c53771fc79c79d3b776e9d14
Jrad Bayrem <bjrad@adaming.fr>
2019-03-29 08:40:12 +0100

    Refoactoring createUser of UserControllerTest and test success

--- src/main/java/com/wemanity/KnowledgeManagement/controller/UserController.java
+++ src/main/java/com/wemanity/KnowledgeManagement/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,7 +48,7 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/createUser", method = RequestMethod.POST)
-	public ResponseEntity<User> createUser(User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 			userService.save(user);
 			return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
