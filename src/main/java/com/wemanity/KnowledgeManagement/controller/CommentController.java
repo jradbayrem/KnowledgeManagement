b8e1436a3be3716c9eb6f9c7971e4460dbcd4e29
Jrad Bayrem <bjrad@adaming.fr>
2019-06-12 18:18:42 +0200

    Refactoring getCommentsByKnowledge Test Success

--- src/main/java/com/wemanity/KnowledgeManagement/controller/CommentController.java
+++ src/main/java/com/wemanity/KnowledgeManagement/controller/CommentController.java
@@ -7,11 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.wemanity.KnowledgeManagement.dto.CommentDto;
 import com.wemanity.KnowledgeManagement.entities.Comment;
@@ -31,7 +27,7 @@ public class CommentController {
 		this.commentService = commentServiceImpl;
 	}
 
-	@RequestMapping(value = "/comments", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/comments")
 	public ResponseEntity<List<CommentDto>> getCommentsByKnowledge(@RequestBody Knowledge knowledge) {
 		List<Comment> comments = this.commentService.findByKnowledge(knowledge);
 		List<CommentDto> commentsDto = new ArrayList<CommentDto>();
