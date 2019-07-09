6cce176936fe20b38f02a1b76fe2d32c16233252
Jrad Bayrem <bjrad@adaming.fr>
2019-07-09 16:47:10 +0200

    Refactoring UserController create Test Success

--- src/main/java/com/wemanity/KnowledgeManagement/controller/UserController.java
+++ src/main/java/com/wemanity/KnowledgeManagement/controller/UserController.java
@@ -3,6 +3,9 @@ package com.wemanity.KnowledgeManagement.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.wemanity.KnowledgeManagement.exceptions.CommentRepositoryException;
+import com.wemanity.KnowledgeManagement.exceptions.UserDtoIsNullException;
+import com.wemanity.KnowledgeManagement.exceptions.UserRepositoryException;
 import com.wemanity.KnowledgeManagement.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,32 +34,40 @@ public class UserController {
 
 	@GetMapping(value = "/users")
 	public List<UserDto> getAllUsers() {
-		List<User> users = userService.findAll();
 		List<UserDto> userDtoList = new ArrayList();
-		users.parallelStream().forEach(
-				currentUser -> {
-					userDtoList.add(userMapper.userToUserDto(currentUser));
-				}
-		);
+		try {
+			List<User> users = userService.findAll();
+			users.parallelStream().forEach(
+					currentUser -> {
+						userDtoList.add(userMapper.userToUserDto(currentUser));
+					}
+			);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 		return userDtoList;
 	}
 
 
 	@PutMapping(value = "/updateUser")
 	public void updateUser(@RequestBody UserDto userDto) {
-		User user ;
 		try {
-			user = userService.generateUserWithRefreshedDataFromUserDto(userDto);
+			User user = userService.generateUserWithRefreshedDataFromUserDto(userDto);
 			userService.update(user);
 		}catch (Exception e){
-
 			e.printStackTrace();
 		}
 	}
 
 	@PostMapping(value = "/createUser")
 	public UserDto createUser(@RequestBody UserDto userDto) {
-			return userMapper.userToUserDto(userService.save(new User(userDto)));
+
+		try {
+			userDto = userMapper.userToUserDto(userService.save(new User(userDto)));
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+			return userDto;
 	}
 	
 }
